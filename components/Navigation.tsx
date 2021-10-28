083c2f5413969a720e33e5527cc3b49a1526b514
manot40 <kevinsandiho290100@gmail.com>
2021-10-28 11:24:10 +0700

    master

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {ViewContext} from './Main';
-import icon from 'react-native-vector-icons/MaterialIcons';
+import Ico from 'react-native-vector-icons/MaterialIcons';
 import {Box, Icon, Text, HStack, ScrollView, Pressable} from 'native-base';
 
 export default ({children}: any) => {
@@ -10,70 +10,72 @@ export default ({children}: any) => {
       <ScrollView flex={1} p="4" backgroundColor="white">
         {children}
       </ScrollView>
-      <HStack bg="darkBlue.600" alignItems="center" safeAreaBottom shadow={6}>
-        <Pressable
-          cursor="pointer"
-          opacity={view === 'Home' ? 1 : 0.5}
-          py="3"
-          flex={1}
-          onPress={() => setView('Home')}>
-          <Center>
-            <Icon mb="1" as={<icon name="home" />} color="white" size="md" />
-            <Text color="white" fontSize="12">
-              Home
-            </Text>
-          </Center>
-        </Pressable>
-        <Pressable
-          cursor="pointer"
-          opacity={view === 'Shuffle' ? 1 : 0.5}
-          py="2"
-          flex={1}
-          onPress={() => setView('Shuffle')}>
-          <Center>
-            <Icon mb="1" as={<icon name="shuffle" />} color="white" size="md" />
-            <Text color="white" fontSize="12">
-              Shuffle
-            </Text>
-          </Center>
-        </Pressable>
-        <Pressable
-          cursor="pointer"
-          opacity={view === 'Leaderboard' ? 1 : 0.6}
-          py="2"
-          flex={1}
-          onPress={() => setView('Leaderboard')}>
-          <Center>
-            <Icon
-              mb="1"
-              as={<icon name="trending-up" />}
-              color="white"
-              size="md"
-            />
-            <Text color="white" fontSize={12}>
-              Leaderboard
-            </Text>
-          </Center>
-        </Pressable>
-        <Pressable
-          cursor="pointer"
-          opacity={view === 'Account' ? 1 : 0.5}
-          py="2"
-          flex={1}
-          onPress={() => setView('Account')}>
-          <Center>
-            <Icon
-              mb="1"
-              as={<icon name="account-circle" />}
-              color="white"
-              size="md"
-            />
-            <Text color="white" fontSize="12">
-              Account
-            </Text>
-          </Center>
-        </Pressable>
-      </HStack>
+      {view && view !== 'Login' && (
+        <HStack bg="darkBlue.600" alignItems="center" safeAreaBottom shadow={6}>
+          <Pressable
+            cursor="pointer"
+            opacity={view === 'Home' ? 1 : 0.5}
+            py="3"
+            flex={1}
+            onPress={() => setView('Home')}>
+            <Center>
+              <Icon mb="1" as={<Ico name="home" />} color="white" size="md" />
+              <Text color="white" fontSize="12">
+                Home
+              </Text>
+            </Center>
+          </Pressable>
+          <Pressable
+            cursor="pointer"
+            opacity={view === 'Shuffle' ? 1 : 0.5}
+            py="2"
+            flex={1}
+            onPress={() => setView('Shuffle')}>
+            <Center>
+              <Icon mb="1" as={<Ico name="shuffle" />} color="white" size="md" />
+              <Text color="white" fontSize="12">
+                Shuffle
+              </Text>
+            </Center>
+          </Pressable>
+          <Pressable
+            cursor="pointer"
+            opacity={view === 'Leaderboard' ? 1 : 0.6}
+            py="2"
+            flex={1}
+            onPress={() => setView('Leaderboard')}>
+            <Center>
+              <Icon
+                mb="1"
+                as={<Ico name="trending-up" />}
+                color="white"
+                size="md"
+              />
+              <Text color="white" fontSize={12}>
+                Leaderboard
+              </Text>
+            </Center>
+          </Pressable>
+          <Pressable
+            cursor="pointer"
+            opacity={view === 'Account' ? 1 : 0.5}
+            py="2"
+            flex={1}
+            onPress={() => setView('Account')}>
+            <Center>
+              <Icon
+                mb="1"
+                as={<Ico name="account-circle" />}
+                color="white"
+                size="md"
+              />
+              <Text color="white" fontSize="12">
+                Account
+              </Text>
+            </Center>
+          </Pressable>
+        </HStack>
+      )}
     </Box>
   );
 };
